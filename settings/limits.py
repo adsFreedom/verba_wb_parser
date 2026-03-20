7cdcab9abcbb783e4c2002e0b11db72f57fc1d0a
adsFreedom <adsfreedomtv@gmail.com>
2026-03-20 18:24:07 +0300

    Add delay settings

---
+++ settings/limits.py
@@ -0,0 +1,32 @@
+"""
+Settings for limits
+
+Raise in min_delay_sec > max_delay_sec
+"""
+from pydantic import BaseModel, model_validator
+from pydantic import BaseSettings
+
+
+class LimitsSettings(BaseModel):
+    """Settings for limits"""
+    min_delay_sec: float = 1
+    max_delay_sec: float = 2
+
+    @model_validator(mode="after")
+    def check_limits(self):
+        if self.min_delay_sec >= self.max_delay_sec:
+            raise ValueError(
+                f"ERROR: min_delay_sec > max_delay_sec "
+                f"({self.min_delay_sec} > {self.max_delay_sec})"
+            )
+        return self
+
+    def __str__(self):
+        """Show settings"""
+        str_list = [
+            "",
+            "Limits",
+            f" - min_delay_sec: {self.min_delay_sec}",
+            f" - max_delay_sec: {self.max_delay_sec}"
+        ]
+        return "\n".join(str_list)
--- settings/settings.py
+++ settings/settings.py
@@ -1,17 +1,30 @@
+"""
+App settings
+
+Load all settings from .env file
+"""
 from pathlib import Path
 
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from settings.limits import LimitsSettings
 
 
 class Settings(BaseSettings):
+    """Load all settings"""
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
         case_sensitive=False,
-        extra="ignore"
+        extra="ignore",
+        env_nested_delimiter="_",
+        env_nested_max_split=1,
     )
     find_string: str = ""
     x_wbaas_token: str = ""
+
+    limits: LimitsSettings = Field(default_factory=LimitsSettings)
 
     def show_settings(self) -> None:
         env_src = self.model_config.get("env_file")
@@ -23,6 +36,8 @@ class Settings(BaseSettings):
         print(f'Loaded settings from "{env_src}":')
         print(f' - {self.find_string=}')
         print(f' - {self.x_wbaas_token=}')
+
+        print(f'{self.limits}')
         print()
 
 
