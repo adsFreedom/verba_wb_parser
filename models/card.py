3bbabec08445fe33d75a226bf699889a5e3b16b5
adsFreedom <adsfreedomtv@gmail.com>
2026-03-23 20:57:40 +0300

    Add default values for card if not load card.json

--- models/card.py
+++ models/card.py
@@ -1,17 +1,20 @@
 from pydantic import BaseModel, Field
 
+
 class Charc(BaseModel):
     """Model for characteristics"""
     name: str = Field(alias="name")
     value: str = Field(alias="value")
 
+
 class GroupOption(BaseModel):
     options: list[Charc] = Field(default_factory=list,
                                  alias="options")
 
+
 class Card(BaseModel):
-    id: int = Field(alias="nm_id")
-    name: str = Field(alias="imt_name")
+    id: int = Field(alias="nm_id", default=0)
+    name: str = Field(alias="imt_name", default="")
     descr: str = Field(alias="description", default="")
     options: list[Charc] = Field(default_factory=list, alias="options")
     group_options: list[GroupOption] = Field(default_factory=list,
