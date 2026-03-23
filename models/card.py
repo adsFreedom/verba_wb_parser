39698ef90fa4ed615eca2f8ee2426ec313e054a3
adsFreedom <adsfreedomtv@gmail.com>
2026-03-23 20:45:52 +0300

    Set default fields in card model

--- models/card.py
+++ models/card.py
@@ -6,11 +6,13 @@ class Charc(BaseModel):
     value: str = Field(alias="value")
 
 class GroupOption(BaseModel):
-    options: list[Charc] = Field(alias="options")
+    options: list[Charc] = Field(default_factory=list,
+                                 alias="options")
 
 class Card(BaseModel):
     id: int = Field(alias="nm_id")
     name: str = Field(alias="imt_name")
-    descr: str = Field(alias="description")
+    descr: str = Field(alias="description", default="")
+    options: list[Charc] = Field(default_factory=list, alias="options")
     group_options: list[GroupOption] = Field(default_factory=list,
                                              alias="grouped_options")
